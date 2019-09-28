4fae04a008ec2324760833bba96b272117f0edd9
calmiyster <callum12345678@hotmail.com>
2019-09-28 16:27:11 +0100

    slide encription

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,6 +8,13 @@ import ImageSpinner from './ImageSpinner';
 import prawn from '../images/gallery/prawnie.jpg';
 import lemongrassFries from '../images/gallery/lemongrass-chicken-fries.jpg';
 import tofuBanhMi from '../images/gallery/tofu-banh-mi.jpg';
+import slide1 from '../images/gallery/slide-1.png' ;
+import slide2 from '../images/gallery/slide-2.png' ;
+import slide3 from '../images/gallery/slide-3.png' ;
+import slide4 from '../images/gallery/slide-4.png' ;
+import slide5 from '../images/gallery/slide-5.png' ;
+import slide6 from '../images/gallery/slide-6.png' ;
+import slide7 from '../images/gallery/slide-7.png';  
 
 const Gallery = () => {
   let images = [tofuBanhMi, lemongrassFries, prawn, banhMi];
