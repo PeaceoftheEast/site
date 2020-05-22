7ab624bcca775105b9194eb0c46b0fc24ed6efdf
calmiyster <callum12345678@hotmail.com>
2020-05-22 12:14:26 +0100

    'pics'

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,14 +14,14 @@ import slide3 from '../images/frontage_1.jpg' ;
 import slide4 from '../images/gallery/newslide1.png' ;
 import slide5 from '../images/gallery/newslide4.png' ;
 import slide6 from '../images/gallery/newslide3.png' ;
-import slide7 from '../images/gallery/slide-7.png' ;  
-import slide8 from '../images/gallery/slide-8.png' ;
+import slide7 from '../images/gallery/newslide5.png' ;  
+import slide8 from '../images/gallery/newslide6.png' ;
 import slide9 from '../images/gallery/slide-9.png' ;
 import slide10 from '../images/gallery/slide-10.png' ;
 import slide11 from '../images/gallery/slide-11.png' ;
 
 const Gallery = () => {
-  let images = [slide1, slide2, slide3, slide8, slide5, slide6, slide7, slide4, slide9, slide10, slide11];
+  let images = [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide11,  ];
   return (
     <div>
       <h2 className="major">Gallery</h2>
